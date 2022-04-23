869ab302761e72191ed6d940ed886d522574b61c
jungai <juworaphol125@gmail.com>
2022-04-23 17:51:21 +0700

    update libs

--- libs/prisma/src/index.ts
+++ libs/prisma/src/index.ts
@@ -1,16 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-import { seedSongs } from "./seed/songs";
-
-const prisma = new PrismaClient();
-
-async function main() {
-	await seedSongs(prisma);
-}
-
-main()
-	.catch((e) => {
-		throw e;
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
-	});
+export * from './libs/prisma.module';
